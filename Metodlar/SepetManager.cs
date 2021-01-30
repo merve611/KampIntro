a9352543a71b552a517c0e3c0cbe366fb425a444
merve <mrveeckrrr@hotmail.com>
2021-01-30 23:54:43 +0300

    Add project files.

---
+++ Metodlar/SepetManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metodlar
+{
+    class SepetManager
+    {
+        //name convertion - isimlendirme kuralı
+        public void Ekle(Urun urun)
+        {
+            Console.WriteLine("Tebrikler. Sepete Eklendi : "+ urun.Adi);
+
+        }
+
+        public void Ekle2(string urunAdi, string aciklama, double fiyat, int stokAdeti)
+        {
+            Console.WriteLine("Tebrikler. Sepete Eklendi : " + urunAdi);
+        }
+    }
+}
---
+++ Metodlar/Urun.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metodlar
+{
+    class Urun
+    {       //prop yazıp iki kez taba basarsak ;
+        //property - özellik
+        public int ıd { get; set; }
+        public string Adi { get; set; }
+        public double Fiyati { get; set; }
+        public string Aciklama { get; set; }
+        public int StokAdeti { get; set; }
+
+
+    }
+}
